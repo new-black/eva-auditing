19452c3879facbb07001f757b377edc5523ff656
Geert ten Napel <gnapel@gmail.com>
2020-03-09 14:37:38 +0100

    feat(Auditing): Added WinForm to solution

---
+++ src/AuditingApp/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AuditingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFicalArchiveURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPublicKeyURL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.tbProgress = new System.Windows.Forms.TextBox();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAndClose = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbFicalArchiveURL
+            // 
+            this.tbFicalArchiveURL.Location = new System.Drawing.Point(119, 6);
+            this.tbFicalArchiveURL.Name = "tbFicalArchiveURL";
+            this.tbFicalArchiveURL.Size = new System.Drawing.Size(447, 23);
+            this.tbFicalArchiveURL.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Archive fiscal URL";
+            // 
+            // tbPublicKeyURL
+            // 
+            this.tbPublicKeyURL.Location = new System.Drawing.Point(119, 35);
+            this.tbPublicKeyURL.Name = "tbPublicKeyURL";
+            this.tbPublicKeyURL.Size = new System.Drawing.Size(447, 23);
+            this.tbPublicKeyURL.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Public key url";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(453, 64);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(113, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbProgress
+            // 
+            this.tbProgress.HideSelection = false;
+            this.tbProgress.Location = new System.Drawing.Point(13, 138);
+            this.tbProgress.Multiline = true;
+            this.tbProgress.Name = "tbProgress";
+            this.tbProgress.ReadOnly = true;
+            this.tbProgress.ShortcutsEnabled = false;
+            this.tbProgress.Size = new System.Drawing.Size(553, 146);
+            this.tbProgress.TabIndex = 0;
+            this.tbProgress.TabStop = false;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(13, 109);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(52, 15);
+            this.lblProgress.TabIndex = 0;
+            this.lblProgress.Text = "Progress";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(302, 290);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAndClose
+            // 
+            this.btnSaveAndClose.Enabled = false;
+            this.btnSaveAndClose.Location = new System.Drawing.Point(383, 290);
+            this.btnSaveAndClose.Name = "btnSaveAndClose";
+            this.btnSaveAndClose.Size = new System.Drawing.Size(102, 23);
+            this.btnSaveAndClose.TabIndex = 5;
+            this.btnSaveAndClose.Text = "Save and close";
+            this.btnSaveAndClose.UseVisualStyleBackColor = true;
+            this.btnSaveAndClose.Click += new System.EventHandler(this.btnSaveAndClose_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(491, 290);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 322);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSaveAndClose);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbPublicKeyURL);
+            this.Controls.Add(this.tbFicalArchiveURL);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.tbProgress);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFicalArchiveURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPublicKeyURL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox tbProgress;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAndClose;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ src/AuditingApp/Form1.cs
@@ -0,0 +1,217 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using EVA.Auditing;
+using EVA.Core.Auditing.Compliancies.NF525;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AuditingApp
+{
+    public partial class Form1 : Form
+    {
+        private List<EventDto> _fiscalArchiveEvents;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void btnSaveAndClose_Click(object sender, EventArgs e) => HandleSaveAndClose(true, true);
+
+        private void btnSave_Click(object sender, EventArgs e) => HandleSaveAndClose(true, false);
+
+        private void btnClose_Click(object sender, EventArgs e) => HandleSaveAndClose(false, true);
+
+        private void HandleSaveAndClose(bool save, bool close)
+        {
+            if (save)
+            {
+
+                // Create dialog
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog
+                {
+                    Filter = "Excel|*.xlsx",
+                    Title = "Save file"
+                };
+
+                // Open dialog
+                saveFileDialog1.ShowDialog();
+
+                // Save data when filename is present
+                if (saveFileDialog1.FileName != "")
+                {
+                    if (File.Exists(saveFileDialog1.FileName))
+                    {
+                        File.Delete(saveFileDialog1.FileName);
+                    }
+
+
+                    using (FileStream fileStream = (FileStream)saveFileDialog1.OpenFile())
+                    using (var spreadsheetDocument = SpreadsheetDocument.Create(fileStream, SpreadsheetDocumentType.Workbook))
+                    {
+                        WorkbookPart workbookPart = spreadsheetDocument.AddWorkbookPart();
+                        Workbook workbook = new Workbook();
+                        workbookPart.Workbook = workbook;
+                        Sheets sheets = new Sheets();
+                        workbook.Append(sheets);
+                        Sheet sheet = new Sheet() { SheetId = 1, Id = "rId1", Name = "Events", };
+                        sheets.Append(sheet);
+                        WorksheetPart worksheetPart = workbookPart.AddNewPart<WorksheetPart>("rId1");
+                        Worksheet worksheet = new Worksheet();
+                        SheetData sheetData = new SheetData();
+                        worksheet.Append(sheetData);
+                        worksheetPart.Worksheet = worksheet;
+
+                        UInt32Value rowIndex = 1;
+                        Row row = new Row() { RowIndex = rowIndex };
+                        row.Append(new Cell() { CellReference = $"A{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"Timestamp") });
+                        row.Append(new Cell() { CellReference = $"B{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"ID") });
+                        row.Append(new Cell() { CellReference = $"C{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"Type") });
+                        row.Append(new Cell() { CellReference = $"D{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"EventCode") });
+                        row.Append(new Cell() { CellReference = $"E{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"EventDescription") });
+                        row.Append(new Cell() { CellReference = $"F{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"Information") });
+
+                        sheetData.Append(row);
+
+                        foreach (var fiscalArchiveEvent in _fiscalArchiveEvents)
+                        {
+                            rowIndex++;
+
+                            row = new Row() { RowIndex = rowIndex };
+                            row.Append(new Cell() { CellReference = $"A{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"{fiscalArchiveEvent.Timestamp:yyyy-MM-dd HH:mm:ss}") });
+                            row.Append(new Cell() { CellReference = $"B{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"{fiscalArchiveEvent.ID}") });
+                            row.Append(new Cell() { CellReference = $"C{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"{fiscalArchiveEvent.Type}") });
+                            row.Append(new Cell() { CellReference = $"D{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"{fiscalArchiveEvent.EventCode}") });
+                            row.Append(new Cell() { CellReference = $"E{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"{fiscalArchiveEvent.EventDescription}") });
+                            row.Append(new Cell() { CellReference = $"F{rowIndex}", DataType = CellValues.String, CellValue = new CellValue($"{fiscalArchiveEvent.Information}") });
+
+                            sheetData.Append(row);
+                        }
+
+
+                        workbookPart.Workbook.Save();
+                        spreadsheetDocument.Close();
+                    }
+                }
+            }
+
+            if (close)
+            {
+                this.Close();
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var loadSuccessfull = true;
+            ArchiveDto fiscalArchiveData = default;
+            string publicKeyData = default;
+
+            btnLoad.Enabled = false;
+            btnSave.Enabled = false;
+            btnSaveAndClose.Enabled = false;
+            btnClose.Enabled = false;
+
+            tbProgress.Clear();
+            tbProgress.Text += $"Starting process ({DateTime.Now:yyyy-MM-dd HH:mm:ss})." + Environment.NewLine + Environment.NewLine;
+
+            try 
+            { 
+                if(string.IsNullOrWhiteSpace(tbFicalArchiveURL.Text))
+                {
+                    tbProgress.Text += "The archive fiscal URL is required." + Environment.NewLine;
+                    loadSuccessfull = false;
+                    return;
+                }
+
+                if (Uri.TryCreate(tbFicalArchiveURL.Text, UriKind.Absolute, out var fiscalArchiveURL) && (fiscalArchiveURL.Scheme == Uri.UriSchemeHttp || fiscalArchiveURL.Scheme == Uri.UriSchemeHttp))
+                {
+                    tbProgress.Text += "The archive fiscal URL is invalid. Process requires a valid http(s) URL." + Environment.NewLine;
+                    loadSuccessfull = false;
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(tbPublicKeyURL.Text))
+                {
+                    tbProgress.Text += "The public key URL is required." + Environment.NewLine;
+                    loadSuccessfull = false;
+                    return;
+                }
+
+                if (Uri.TryCreate(tbPublicKeyURL.Text, UriKind.Absolute, out var publicKeyURL) && (publicKeyURL.Scheme == Uri.UriSchemeHttp || publicKeyURL.Scheme == Uri.UriSchemeHttp))
+                {
+                    tbProgress.Text += "The public key URL is invalid. Process requires a valid http(s) URL." + Environment.NewLine;
+                    loadSuccessfull = false;
+                    return;
+                }
+
+                // Get fiscal archive
+                await Task.WhenAll(
+                    Utility.DownloadArchive(fiscalArchiveURL.AbsoluteUri).ContinueWith(t =>
+                    {
+                        if (!t.Result.IsSuccessful)
+                        {
+                            tbProgress.Text += "The download of the fiscal archive failed with the following error:" + Environment.NewLine;
+                            tbProgress.Text += t.Result.Message + Environment.NewLine;
+                            loadSuccessfull = false;
+                        }
+
+                        fiscalArchiveData = t.Result.Data;
+                    }),
+                    Utility.DownloadKey(publicKeyURL.AbsoluteUri).ContinueWith(t =>
+                    {
+                        if (!t.Result.IsSuccessful)
+                        {
+                            tbProgress.Text += "The download of the public key failed with the following error:" + Environment.NewLine;
+                            tbProgress.Text += t.Result.Message + Environment.NewLine;
+                            loadSuccessfull = false;
+                            return;
+                        }
+
+                        publicKeyData = t.Result.Data;
+                    })
+                );
+
+                if (loadSuccessfull)
+                {
+                    // Validate
+                    var validationResult = Utility.Verify(fiscalArchiveData, publicKeyData).Result;
+                    if (!validationResult.IsSuccessful)
+                    {
+                        tbProgress.Text += "Fiscal archive contains errors:" + Environment.NewLine;
+                        foreach (var error in validationResult.Errors)
+                        {
+                            tbProgress.Text += $"- {error}" + Environment.NewLine;
+                        }
+                    }
+
+                    this._fiscalArchiveEvents = fiscalArchiveData.Events;
+                    loadSuccessfull = true;
+                }
+            }
+            catch(Exception excep)
+            {
+                var exceptionId = DateTime.Now.ToString("yyyyMMddhhmmss") + Guid.NewGuid().ToString().Split('-')[0];
+
+                File.AppendAllLines("ErrorLog.log", new List<string>() { $"{DateTime.Now:yyyy-MM-dd hh:mm:ss};UnexpectedError;{excep.Message}" });
+                tbProgress.Text += $"An unexpected error occured. See error log for details (exceptionId '{exceptionId}')";
+            }
+            finally
+            {
+                tbProgress.Text += Environment.NewLine + $"Process completed ({DateTime.Now:yyyy-MM-dd HH:mm:ss}).";
+                btnLoad.Enabled = true;
+                btnClose.Enabled = true;
+
+                if (loadSuccessfull)
+                {
+                    btnSave.Enabled = true;
+                    btnSaveAndClose.Enabled = true;
+                }
+            }
+        }
+    }
+}
